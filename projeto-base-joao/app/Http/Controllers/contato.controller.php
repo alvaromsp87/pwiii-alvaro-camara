be2d8f44e60e5366c845460b2f81cf772ac5cef9
alvaro <alvaro.camara@etec.sp.gov.br>
2025-02-21 22:35:17 -0300

    Aula 21-02

---
+++ projeto-base-joao/app/Http/Controllers/contato.controller.php
@@ -0,0 +1,10 @@
+<?php
+
+use app\models\contatos;
+use Illuminate\Http\Request;
+
+
+class contatoController extends Controller
+{
+    
+}
---
+++ projeto-base-joao/database/migrations/2025_02_22_010910_contatos.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contatos', function (Blueprint $table) {
+            
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('e-mail');
+            $table->text('comentario')->nullable();
+            $table->enum('status', ['ativo', 'desativado']);
+            $table->string('telefone')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
